c2ba5d5b1e81dae09db757d03262436e287c8f7e
LORENZO TRUJILLO ROJAS <42102130@uaz.edu.mx>
2023-01-31 09:30:45 -0600

    Definicion de los metodos de ListaEstatica

--- src/estructurasLineales/ListaEstatica.java
+++ src/estructurasLineales/ListaEstatica.java
@@ -1,7 +1,33 @@
 package estructurasLineales;
 
-public class ListaEstatica {
+public class ListaEstatica implements VectorLista{
     protected int tope;
     protected int MAXIMO;
-    protected Object informacion;
+    private final int VACIO=-1;
+    protected Object[] informacion;
+
+    public ListaEstatica(int tamanio){
+        this.MAXIMO=tamanio;
+        this.informacion=new Object[tamanio];
+        this.tope=-1;
+    }
+
+    @Override
+    public boolean vacia() {
+        return this.tope == this.VACIO;
+    }
+
+    @Override
+    public boolean llena() {
+        return this.tope == this.MAXIMO-1;
+    }
+
+    @Override
+    public int agregar(Object info){
+        if(llena()) return -1;
+        this.tope+=1;
+        this.informacion[tope]=info;
+        return this.tope;
+
+    }
 }
